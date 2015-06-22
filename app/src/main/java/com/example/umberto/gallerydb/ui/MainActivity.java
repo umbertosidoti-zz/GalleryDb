7a5c5ee02ad0818fd1d05fa362bee4239936752f
umberto.sidoti <umbertosidoti@gmail.com>
2015-06-22 23:13:28 +0100

    -changed package

--- app/src/main/java/com/example/umberto/gallerydb/ui/MainActivity.java
+++ app/src/main/java/com/example/umberto/gallerydb/ui/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.umberto.gallerydb;
+package com.example.umberto.gallerydb.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.umberto.gallerydb.R;
 
 public class MainActivity extends AppCompatActivity {
 
