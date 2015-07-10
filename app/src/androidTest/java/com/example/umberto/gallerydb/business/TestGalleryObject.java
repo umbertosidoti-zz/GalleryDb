60c4d684a698069cdb28131ac2a5581a53e61a7c
umberto.sidoti <umbertosidoti@gmail.com>
2015-07-10 18:37:00 +0100

    -added test for GalleryObject

---
+++ app/src/androidTest/java/com/example/umberto/gallerydb/business/TestGalleryObject.java
@@ -0,0 +1,20 @@
+package com.example.umberto.gallerydb.business;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Umberto Sidoti on 10/07/2015.
+ */
+public class TestGalleryObject extends AndroidTestCase {
+
+    public void testGalleryObjectConstructor(){
+        GalleryObject obj1= new GalleryObject();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        GalleryObject obj2= new GalleryObject();
+        assertTrue(obj2.getCreationDate()>obj1.getCreationDate());
+    }
+}
