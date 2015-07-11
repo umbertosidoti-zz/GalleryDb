7333f385abc2dfb6611e0f00769e0c9d295ef5e6
umberto.sidoti <umbertosidoti@gmail.com>
2015-07-11 12:11:37 +0100

    -added test for load all

---
+++ app/src/androidTest/java/com/example/umberto/gallerydb/task/TestLoadAllTask.java
@@ -0,0 +1,67 @@
+package com.example.umberto.gallerydb.task;
+
+import android.test.InstrumentationTestCase;
+
+import com.example.umberto.gallerydb.GalleryApplication;
+import com.example.umberto.gallerydb.business.interfaces.GenericObject;
+import com.example.umberto.gallerydb.mockObject.MockServiceLocator;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Umberto Sidoti on 11/07/2015.
+ */
+public class TestLoadAllTask extends InstrumentationTestCase {
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        GalleryApplication.getInstance().setServiceLocator(new MockServiceLocator());
+    }
+
+    public void testLoadAll() {
+
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        try {
+            runTestOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    //noinspection unchecked
+                    new LoadAllGenericObject() {
+                        @Override
+                        protected void onPostExecute(ArrayList<GenericObject> genericObjects) {
+
+                            assertNotNull(genericObjects);
+                            assertEquals(10, genericObjects.size());
+                            assertTrue(checkIfExist(genericObjects, 1) != -1);
+                            assertTrue(checkIfExist(genericObjects, 5)!=-1);
+                            signal.countDown();
+                        }
+                    }.execute();
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        try {
+            signal.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private int checkIfExist(ArrayList<GenericObject> genericObjects, int id) {
+        int index = -1;
+        int size = genericObjects.size();
+        for (int i = 0; (i < size) && (index == -1); i++) {
+            GenericObject obj = genericObjects.get(i);
+            if (obj.getId() == id)
+                index = i;
+        }
+        return index;
+    }
+}
